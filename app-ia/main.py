bb0b2ff728bdfc9a5c410a04568fac52569f735a
overexpOG <ignacio.alvbah@gmail.com>
2025-05-14 22:14:03 -0400

    feat: Se hace la conexión entre la extension y el servidor.

--- app-ia/main.py
+++ app-ia/main.py
@@ -5,6 +5,10 @@ from flask import Flask, jsonify, request
 
 @app.route('/verificarCensura', methods=['POST'])
 def verificarCensura():
+    api_key = request.headers.get('X-Api-Key')
+    if api_key != 'e55d7f49-a705-4895-bf5f-d63aa1f46e11':
+        return jsonify({'error': 'No autorizado'}), 401
+
     tweet = request.json
     print("se recibe: {}".format(tweet))
     texto = tweet['texto']
